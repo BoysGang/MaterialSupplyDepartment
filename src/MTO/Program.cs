9155ef2cabf4c45bb2e572ba8459c5c4fc94f9fd
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2021-04-01 19:21:01 +0700

    ORM and DB connection were configured

--- src/MTO/Program.cs
+++ src/MTO/Program.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MTO.Models;
 
 namespace MTO
 {
+
     static class Program
     {
+        public static MTOContext db;
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            db = new MTOContext();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormLogin());
