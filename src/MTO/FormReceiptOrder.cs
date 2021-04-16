74a0a0da47dc9bd9f50db753783676a090d396c9
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2021-04-16 15:17:35 +0700

    Add find receipt orders

--- src/MTO/FormReceiptOrder.cs
+++ src/MTO/FormReceiptOrder.cs
@@ -9,11 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using MTO.Models;
+using MTO.Utils;
 
 namespace MTO
 {
     public partial class FormReceiptOrder : Form
     {
+        private List<ReceiptOrder> receiptOrders;
+
         public FormReceiptOrder()
         {
             InitializeComponent();
@@ -21,14 +24,14 @@ namespace MTO
 
         private void dateTimePicker3_ValueChanged(object sender, EventArgs e)
         {
-            dateTimePicker3.CustomFormat = "dd/MM/yyyy";
+            dtp_deliveryDate.CustomFormat = "dd/MM/yyyy";
         }
 
         private void dateTimePicker3_KeyDown(object sender, KeyEventArgs e)
         {
             if ((e.KeyCode == Keys.Back) || (e.KeyCode == Keys.Delete))
             {
-                dateTimePicker3.CustomFormat = " ";
+                dtp_deliveryDate.CustomFormat = " ";
             }
         }
 
@@ -84,11 +87,38 @@ namespace MTO
             dgv_receiptOrders.AutoGenerateColumns = false;
             dgv_receiptOrders.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
 
+            List<Warehouse> warehouses = Program.db.Warehouses.ToList();
+            cb_warehouse.DataSource = warehouses;
+            cb_warehouse.SelectedIndex = -1;
+
+            List<Provider> providers = Program.db.Providers.ToList();
+            cb_provider.DataSource = providers;
+            cb_provider.SelectedIndex = -1;
+
+            List<Contract> contracts = Program.db.Contracts.ToList();
+            AutoCompleteStringCollection contractSource = new AutoCompleteStringCollection();
+            foreach (Contract contract in contracts)
+                contractSource.Add(contract.ContractNumber);
+            tb_contractNumber.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            tb_contractNumber.AutoCompleteMode = AutoCompleteMode.Suggest;
+            tb_contractNumber.AutoCompleteCustomSource = contractSource;
+
+            List<ReceiptOrder> receiptOrders = Program.db.ReceiptOrders.ToList();
+            AutoCompleteStringCollection ordersSource = new AutoCompleteStringCollection();
+            foreach (ReceiptOrder order in receiptOrders)
+                ordersSource.Add(order.ReceiptOrderNumber);
+            tb_receiptOrderNumber.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            tb_receiptOrderNumber.AutoCompleteMode = AutoCompleteMode.Suggest;
+            tb_receiptOrderNumber.AutoCompleteCustomSource = ordersSource;
+
             if (Program.user.isAccounting())
             {
                 tsmi_contracts.Visible = false;
             }
 
+            this.receiptOrders = receiptOrders;
+
+            updateReceiptOrderTable();
         }
 
         private void btn_viewReceiptOrder_Click(object sender, EventArgs e)
@@ -102,15 +132,11 @@ namespace MTO
             }
         }
 
-        private void FormReceiptOrder_Activated(object sender, EventArgs e)
-        {
-            updateReceiptOrderTable();
-        }
-
         private void updateReceiptOrderTable()
         {
-            List<ReceiptOrder> orders = Program.db.ReceiptOrders.ToList();
-            dgv_receiptOrders.DataSource = orders;
+            findReceiptOrders();
+
+            dgv_receiptOrders.DataSource = receiptOrders;
 
             dgv_receiptOrders.Columns[0].DataPropertyName = "PK_ReceiptOrder";
             dgv_receiptOrders.Columns[1].DataPropertyName = "ReceiptOrderNumber";
@@ -137,6 +163,94 @@ namespace MTO
             int PK_ReceiptOrder = Convert.ToInt32(dgv_receiptOrders.SelectedRows[0].Cells[0].Value);
 
             return Program.db.ReceiptOrders.Find(PK_ReceiptOrder);
+        }
+
+        private void tb_receiptOrder_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!ModifierKeys.HasFlag(Keys.Control))
+                e.Handled = !TextValidator.isNumber(e.KeyChar.ToString());
+        }
+
+        private void tb_contract_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!ModifierKeys.HasFlag(Keys.Control))
+                e.Handled = !TextValidator.isNumber(e.KeyChar.ToString());
+        }
+
+        private void btn_resetSearch_Click(object sender, EventArgs e)
+        {
+            tb_receiptOrderNumber.Text = "";
+            cb_provider.SelectedIndex = -1;
+            dtp_deliveryDate.CustomFormat = " ";
+            cb_warehouse.SelectedIndex = -1;
+            tb_contractNumber.Text = "";
+        }
+
+        private void btn_findReceiptOrder_Click(object sender, EventArgs e)
+        {
+            updateReceiptOrderTable();
+        }
+
+        private void findReceiptOrders()
+        {
+            List<ReceiptOrder> foundReceiptOrders = new List<ReceiptOrder>();
+
+            bool orderNumberCriterium = tb_receiptOrderNumber.Text != string.Empty;
+            string orderNumber = tb_receiptOrderNumber.Text.ToLower();
+
+            Provider provider = (Provider)cb_provider.SelectedItem;
+            bool providerCriterium = false;
+            int PK_Provider = -1;
+            if (provider != null)
+            {
+                providerCriterium = true;
+                PK_Provider = provider.PK_Provider;
+            }
+
+            bool deliveryCriterium = dtp_deliveryDate.CustomFormat.ToString() != " ";
+            string deliveryDate = dtp_deliveryDate.Value.ToString("dd-MM-yyyy");
+
+            Warehouse warehouse = (Warehouse)cb_warehouse.SelectedItem;
+            bool warehouseCriterium = false;
+            int PK_Warehouse = -1;
+            if (warehouse != null)
+            {
+                warehouseCriterium = true;
+                PK_Warehouse = warehouse.PK_Warehouse;
+            }
+
+            bool contractNumberCriterium = tb_contractNumber.Text != string.Empty;
+            string contractNumber = tb_contractNumber.Text.ToLower();
+
+            foreach (ReceiptOrder order in Program.db.ReceiptOrders)
+            {
+                bool orderNumberFound = !orderNumberCriterium;
+                bool providerFound = !providerCriterium;
+                bool deliveryFound = !deliveryCriterium;
+                bool warehouseFound = !warehouseCriterium;
+                bool contractNumberFound = !contractNumberCriterium;
+
+                if (orderNumberCriterium && order.ReceiptOrderNumber.Contains(orderNumber))
+                    orderNumberFound = true;
+
+                if (providerCriterium && order.Provider.PK_Provider == PK_Provider)
+                    providerFound = true;
+
+                if (deliveryCriterium && order.DeliveryDateWithoutTime.Contains(deliveryDate))
+                    deliveryFound = true;
+
+                if (warehouseCriterium && order.Warehouse.PK_Warehouse == PK_Warehouse)
+                    warehouseFound = true;
+
+                if (contractNumberCriterium && order.Contract.ContractNumber.Contains(contractNumber))
+                    contractNumberFound = true;
+
+                if (orderNumberFound && providerFound && deliveryFound && 
+                    warehouseFound && contractNumberFound)
+                    foundReceiptOrders.Add(order);
+            }
+
+            receiptOrders = foundReceiptOrders;
         }
     }
 }
