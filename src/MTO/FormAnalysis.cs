d05fe2183e65981ab2955f63d820805b2de9c614
Evster <8961240@gmail.com>
2021-04-20 22:11:25 +0700

    wrong

--- src/MTO/FormAnalysis.cs
+++ src/MTO/FormAnalysis.cs
@@ -28,7 +28,15 @@ namespace MTO
                 + (contract.IsOpened ? "ОТКРЫТ)" : "ЗАКРЫТ)");
 
 
+
+            int pk_contract = contract.PK_Contract;
             lines = contract.getContractLines();
+            List<ReceiptOrderLine> receiptLines = Program.db.ReceiptOrderLines
+                                            .Where(b => b.ReceiptOrder.PK_Contract == pk_contract)
+                                            //.OrderBy(b => b.ReceiptOrder.DeliveryDate)
+                                            .ToList();
+
+
             int linesCount = lines.Count;
             ContractLine line;
 
@@ -38,18 +46,43 @@ namespace MTO
 
                 line = lines[i];
 
+
+                ReceiptOrderLine thisLine = receiptLines.FindAll(b => b.PK_Resource == line.PK_Resource)
+                    .Where(b => b.ReceiptOrder.DeliveryDate < line.DeliveryDate)
+                    .FirstOrDefault();
+
+
+                if(thisLine != null)
+                {
+                    DateTime dateFact = thisLine.ReceiptOrder.DeliveryDate;
+                    float amountFact = thisLine.AcceptedAmount;
+
+
+                    dgv_analysis.Rows[i].Cells[6].Value = dateFact.ToString("dd-MM-yyyy");
+                    dgv_analysis.Rows[i].Cells[8].Value = thisLine == null ? "0" : thisLine.AcceptedAmount.ToString();
+                    dgv_analysis.Rows[i].Cells[9].Value = line.Amount - thisLine.AcceptedAmount;
+                    dgv_analysis.Rows[i].Cells[11].Value = line.UnitPrice * (decimal)(line.Amount - thisLine.AcceptedAmount);
+
+                    receiptLines.Remove(thisLine);
+                }
+                else
+                {
+                    dgv_analysis.Rows[i].Cells[6].Value = "-";
+                    dgv_analysis.Rows[i].Cells[8].Value = 0;
+                    dgv_analysis.Rows[i].Cells[9].Value = line.Amount;
+                    dgv_analysis.Rows[i].Cells[11].Value = (decimal)line.Amount * line.UnitPrice;
+
+                }
+
+
                 dgv_analysis.Rows[i].Cells[0].Value = line.PK_ContractLine;
                 dgv_analysis.Rows[i].Cells[1].Value = line.Resource;
                 dgv_analysis.Rows[i].Cells[2].Value = line.Resource_Cipher;
                 dgv_analysis.Rows[i].Cells[3].Value = line.Resource_Unit;
                 dgv_analysis.Rows[i].Cells[4].Value = line.Resource_Cipher;
                 dgv_analysis.Rows[i].Cells[5].Value = line.DeliveryDate.ToString("dd-MM-yyyy");
-                dgv_analysis.Rows[i].Cells[6].Value = "";
                 dgv_analysis.Rows[i].Cells[7].Value = line.Amount;
-                dgv_analysis.Rows[i].Cells[8].Value = "";
-                dgv_analysis.Rows[i].Cells[9].Value = "";
                 dgv_analysis.Rows[i].Cells[10].Value = line.UnitPrice;
-                dgv_analysis.Rows[i].Cells[11].Value = "";
             }
 
 
