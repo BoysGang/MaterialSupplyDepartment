c58d06a74ff0a120a8d24a361fd09e05095e8252
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2021-04-22 15:54:03 +0700

    Add big data generation backbone

---
+++ src/MTO/Utils/BigDataGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MTO;
+using MTO.Models;
+
+namespace MTO.Utils
+{
+    class BigDataGenerator
+    {
+        private MTOContext db;
+
+        public BigDataGenerator(MTOContext db)
+        {
+            this.db = db;
+        }
+
+        public bool generateContracts()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool generateDictionaries()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool generate()
+        {
+            return true;
+        }
+    }
+}
