640650a5d8b585f23005fda4305bd100990442d9
Evster <8961240@gmail.com>
2021-04-22 19:20:50 +0700

    added contract and contractlines generations

--- src/MTO/Utils/BigDataGenerator.cs
+++ src/MTO/Utils/BigDataGenerator.cs
@@ -17,13 +17,123 @@ namespace MTO.Utils
         public BigDataGenerator(MTOContext db)
         {
             this.db = db;
+
+            startDateDefault = new DateTime(DateTime.Now.Year - 3, 1, 1);
+            finalDateDefault = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            daysRange = (finalDateDefault - startDateDefault).Days;
         }
+
+        private Random rand = new Random();
+
+        private DateTime startDateDefault;
+        private DateTime finalDateDefault;
+        private int daysRange;
+
+
+        private DateTime randomDay()
+        {
+            return startDateDefault.AddDays(rand.Next(daysRange) + 1);
+        }
+
+        private DateTime randomDay(DateTime dateFrom)
+        {
+            return dateFrom.AddDays(rand.Next((finalDateDefault - dateFrom).Days + 1));
+        }
+
+        private DateTime randomDay(DateTime dateFrom, DateTime dateTo)
+        {
+            return dateFrom.AddDays(rand.Next((dateTo - dateFrom).Days + 1));
+        }
+
 
         public bool generateContracts()
         {
             try
             {
+                int amountCities = cities.Length;
+                int amountRoles = roles.Length;
 
+                OrganizationDescription description = db.OrganizationDescriptions.FirstOrDefault();
+                if (description == null)
+                    return false;
+
+                List<int> PK_Providers = new List<int>();
+                int amountProviders = PK_Providers.Count;
+
+                List<int> PK_Resources = new List<int>();
+                int amountResources = PK_Resources.Count;
+
+                foreach (var provider in db.Providers)
+                {
+                    PK_Providers.Add(provider.PK_Provider);
+                }
+
+                foreach (var resource in db.Resources)
+                {
+                    PK_Resources.Add(resource.PK_Resource);
+                }
+
+
+                List<ContractLine> generatedContractLines = new List<ContractLine>();
+                DateTime conclusionDate, startDate, expiredDate;
+
+                //генерация контрактов и ордеров
+                for (int i = 0; i < 500; i++)
+                {
+
+                    conclusionDate = randomDay();
+                    startDate = conclusionDate.AddDays(1);
+                    expiredDate = randomDay(startDate);
+
+                    //генерация контракта
+                    Contract generatedContract = new Contract()
+                    {
+                        ContractNumber = "0000" + (i + 1).ToString(),
+
+                        ConclusionDate = conclusionDate,
+                        StartDate = startDate,
+                        ExpiredDate = expiredDate,
+
+                        ConclusionCity = cities[rand.Next(amountCities)],
+                        ProviderAgentName = "Оформитель_" + (i + 1).ToString(),
+                        ProviderAgentRole = roles[rand.Next(amountRoles)],
+                        CustomerAgentName = "Оформитель_" + (i + 1).ToString(),
+                        CustomerAgentRole = roles[rand.Next(amountRoles)],
+
+                        SupplierPenalty = Decimal.Parse(rand.Next(10) + rand.NextDouble().ToString()),
+                        CustomerPenalty = Decimal.Parse(rand.Next(10) + rand.NextDouble().ToString()),
+                        IsOpened = expiredDate <= DateTime.Now,
+                        PK_Provider = PK_Providers[rand.Next(amountProviders)],
+                        PK_OrganizationDescription = description.PK_OrganizationDescription,
+                    };
+                    db.Contracts.Add(generatedContract);
+                    db.SaveChanges();
+
+                    //генерация строк контракта
+                    generatedContractLines.Clear();
+                    int pk_contract = generatedContract.PK_Contract;
+                    int amountContractLines = rand.Next(10);
+                    for(int j = 0; j < amountContractLines; j++)
+                    {
+                        ContractLine line = new ContractLine()
+                        {
+                            PK_Contract = pk_contract,
+                            PK_Resource = PK_Resources[rand.Next(amountResources)],
+                            Amount = rand.Next(1, 100),
+                            UnitPrice = Decimal.Parse(rand.Next(1, 10000).ToString()),
+                            DeliveryDate = randomDay(startDate, expiredDate),
+                        };
+                        db.ContractLines.Add(line);
+                        generatedContractLines.Add(line);
+                    }
+                    db.SaveChanges();
+
+                    //генерация приходных ордеров
+
+                    //генерация строк приходных ордеров
+
+
+                }
             }
             catch (Exception ex)
             {
@@ -34,29 +144,33 @@ namespace MTO.Utils
             return true;
         }
 
+        private string[] cities = new string[]
+        {
+            "Токио",
+            "Барнаул",
+            "Артем",
+            "Бийск",
+            "Москва",
+            "Грозный",
+            "Киров",
+            "Новочеркасск",
+            "Новосибирск",
+            "Новороссийск",
+        };
 
-        private Dictionary<int, string> cities = new Dictionary<int, string>
-            {
-                {0,  "Максим"},
-                {1,  "Барнаул"},
-                {2,  "Артем"},
-                {3,  "Бийск"},
-                {4,  "Москва"},
-                {5,  "Грозный"},
-                {6,  "Киров"},
-                {7,  "Новочеркасск"},
-                {8,  "Новосибирск"},
-                {9,  "Новороссийск"},
-                {10,  "Новокузнецк"},
-            };
+        private string[] roles = new string[]
+        {
+            "директор",
+            "исп. директор",
+            "фин. директор",
+            "бухгалтер",
+            "провизор"
+        };
 
         public bool generateDictionaries()
         {
             try
             {
-                Random rand = new Random();
-
-
                 OrganizationDescription description = new OrganizationDescription()
                 {
                     PK_OrganizationDescription = 1,
@@ -73,6 +187,7 @@ namespace MTO.Utils
                     BIK = rand.Next(10000, 99999).ToString() + rand.Next(1000, 9999).ToString(),
 
                 };
+                db.OrganizationDescriptions.Add(description);
                 db.SaveChanges();
 
                 //создание типа ресурса
